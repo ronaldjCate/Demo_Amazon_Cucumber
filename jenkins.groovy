d6ba41f089a08413d01ffbeed6c4d5248fee119f
RonalCate <ronaldjcate@gmail.com>
2023-12-28 12:50:30 -0500

    second commit

---
+++ jenkins.groovy
@@ -0,0 +1,32 @@
+pipeline{
+agent any
+stages{
+    stage('Compile Stage'){
+        steps{
+           script{
+               def mvnHome = tool name: 'maven_3_9_5', type: 'maven'
+               withEnv(["PATH+MAVEN=${mvnHome}/bin"]){
+                   bat "${mvnHome}\\bin\\mvn clean compile"
+               }
+           }
+        }
+    }
+    stage('Test Stage'){
+        steps{
+            script{
+                def mvnHome = tool name: 'maven_3_9_5',type: 'maven'
+                withEnv(["PATH+MAVEN=${mvnHome}/bin"]){
+                    bat "${mvnHome}\\bin\\mvn clean verify -Dcucumber.filter.tags=\"@prueba1\""
+                }
+            }
+        }
+    }
+    stage ('Cucumber Reports'){
+        steps{
+            cucumber buildStatus: "UNSTABLE",
+            fileIncludePattern: "**/cucumber.json",
+            jsonReportDirectory: 'target'
+        }
+    }
+}
+}
