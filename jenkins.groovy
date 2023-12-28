99d979c991ed0a9f45fd43d1052e91fc9b69d1a1
RonalCate <ronaldjcate@gmail.com>
2023-12-28 18:13:50 -0500

    dec commit

--- jenkins.groovy
+++ jenkins.groovy
@@ -5,40 +5,33 @@ def defDate = new Date()
 def defTimestamp = defDateFormat.format(defDate).toString()
 pipeline{
     agent any
-    tools {
-        maven 'M3'
-        jdk 'JAVA21'
-    }
-
-    stages {
-
-        stage ('Build') {
-            steps {
-                bat ("mvn clean install -DskipTests")
-                bat ("mvn clean verify")
-            }
-        }
-
-        stage ('Ejecutar Pruebas') {
-            steps {
-                script {
-                    try {
-                        bat("mvn clean verify -Dcucumber.features=src/test/resources/features -Dcucumber.filter.tags=${tags} -Dcucumber.plugin=json:target/build/cucumber.json -Dcucumber.glue=bdd.stepDefinition")
-                        echo 'Ejecucion de pruebas sin errores...'
-                    }
-                    catch (ex) {
-                        echo 'Finalizo ejecucion con fallos...'
-                        error ('Failed')
+    stages{
+        stage('Compile Stage'){
+            steps{
+                script{
+                    def mvnHome = tool name: 'maven_3_9_5', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]){
+                        bat "${mvnHome}\\bin\\mvn clean compile"
                     }
                 }
             }
         }
-    stage ('Cucumber Reports'){
-        steps{
-            cucumber buildStatus: "UNSTABLE",
-            fileIncludePattern: "**/cucumber.json",
-            jsonReportDirectory: 'target'
+        stage('Test Stage'){
+            steps{
+                script{
+                    def mvnHome = tool name: 'maven_3_9_5',type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]){
+                        bat "${mvnHome}\\bin\\mvn clean verify -Dcucumber.features=src/test/resources/features -Dcucumber.filter.tags=\"${tags}\" -Dcucumber.plugin=json:target/build/cucumber.json -Dcucumber.glue=bdd.stepDefinition"
+                    }
+                }
+            }
+        }
+        stage ('Cucumber Reports'){
+            steps{
+                cucumber buildStatus: "UNSTABLE",
+                        fileIncludePattern: "**/cucumber.json",
+                        jsonReportDirectory: 'target'
+            }
         }
     }
-}
 }
